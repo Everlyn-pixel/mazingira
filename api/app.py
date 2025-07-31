f0b6b625a9804bf9e71e313d9f3a85ee22351100
MarkMusonye <markmusonye@gmail.com>
2025-08-01 00:31:57 +0300

    cors......

--- api/app.py
+++ api/app.py
@@ -1,3 +1,26 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+from extensions import db, jwt
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    CORS(app, resources={r"/api/*": {"origins": "*", "allow_headers": "*", "methods": "*"}})
+    db.init_app(app)
+    Migrate(app, db)
+    jwt.init_app(app)
+
+    from routes.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
+
+    return app
+
 from app import create_app
 from extensions import db
 from models.user import User
@@ -28,5 +51,4 @@ app.register_blueprint(beneficiaries_blueprint)
 def make_shell_context():
     return {'db': db, 'User': User, 'Organization': Organization, 'Donation': Donation, 'Story': Story, 'Beneficiary': Beneficiary}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = create_app()
